8a988a42c16d96b7c2d221c8fa9363a1aecba36e
root <root@DESKTOP-7S6CRQL.localdomain>
2018-09-28 15:26:52 +0200

    Finish ReactWorkshop

--- geekabyte-workshop/src/App.js
+++ geekabyte-workshop/src/App.js
@@ -1,55 +1,65 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
-import axios from "axios" 
-import { nowShowingUrl, topRatedUrl } from "./api/apiConfig"
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { nowShowingUrl, topRatedUrl } from "./api/apiConfig";
 
 // file imports
-import Tabbar from "./movie-island/Tabbar"
+import Tabbar from "./movie-island/Tabbar";
+import List from "./movie-island/List";
 
 class App extends Component {
-	constructor(props) {
-		super(props)
+  constructor(props) {
+    super(props);
+    this.state = { selectedType: "now_showing" };
 
-		// Bind events
-		this.onTabSelected = this.onTabSelected.bind(this)
-	}
+    // Bind events
+    this.onTabSelected = this.onTabSelected.bind(this);
+  }
 
-	onTabSelected(selectedType) {
-		// AJAX call to get the API response
-		switch (selectedType) {
-			case "now_showing":
-				axios.get(nowShowingUrl).then(response => {
-					this.setState({
-						nowShowing: response.data.results,
-						selectedType,
-					})
-				})
-				break
-			case "top_rated":
-				axios.get(topRatedUrl).then(response => {
-					this.setState({
-						topRated: response.data.results,
-						selectedType,
-					})
-				})
-				break
-		}
-	}
+  componentDidMount() {
+    this.onTabSelected(this.state.selectedType);
+  }
 
+  onTabSelected(selectedType) {
+    // AJAX call to get the API response
+    switch (selectedType) {
+      case "now_showing":
+        axios.get(nowShowingUrl).then(response => {
+          this.setState({
+            nowShowing: response.data.results,
+            selectedType
+          });
+        });
+        break;
+      case "top_rated":
+        axios.get(topRatedUrl).then(response => {
+          this.setState({
+            topRated: response.data.results,
+            selectedType
+          });
+        });
+        break;
+    }
+  }
 
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<h1 className="App-title">Welcome to Movie-island</h1>
-					<img src={logo} className="App-logo" alt="logo" />
-				</header>
-				<p className="App-intro">Click on the Following Tabs</p>
-				<Tabbar onTabSelected={this.onTabSelected} />
-			</div>
-		)
-	}
+  render() {
+    const { selectedType, nowShowing, topRated } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Movie-island</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <p className="App-intro">Click on the Following Tabs</p>
+        <Tabbar onTabSelected={this.onTabSelected} />
+        {selectedType === "now_showing" &&
+          nowShowing && <List data={nowShowing} />}
+        {selectedType === "top_rated" && topRated && <List data={topRated} />}
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- geekabyte-workshop/src/movie-island/Tabbar.js
+++ geekabyte-workshop/src/movie-island/Tabbar.js
@@ -1,55 +1,55 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
 // TabBar component renders Types of list to be rendered,
 // Whether list if for now showing or top rated movies
 class TabBar extends Component {
-	constructor(props) {
-		super(props)
+  constructor(props) {
+    super(props);
 
-		// bind events
-		this.onTabChange = this.onTabChange.bind(this)
+    // bind events
+    this.onTabChange = this.onTabChange.bind(this);
 
-		// initial state
-		this.state = { selectedType: "now_showing" }
-	}
+    // initial state
+    this.state = { selectedType: "now_showing" };
+  }
 
-	onTabChange(event) {
-		const selectedType = event.target.getAttribute("data-type")
+  onTabChange(event) {
+    const selectedType = event.target.getAttribute("data-type");
 
-		this.setState({
-			selectedType, // No need of writing selectedType : selectedType // ES6 syntax
-		})
+    this.setState({
+      selectedType // No need of writing selectedType : selectedType // ES6 syntax
+    });
 
-		// Let the parent know about the changes
-		if (this.props.onTabSelected) {
-			this.props.onTabSelected(selectedType)
-		}
-	}
+    // Let the parent know about the changes
+    if (this.props.onTabSelected) {
+      this.props.onTabSelected(selectedType);
+    }
+  }
 
-	render() {
-		const { selectedType } = this.state,
-			isNowShowingSelected = selectedType === "now_showing",
-			isTopRatedSelected = selectedType === "top_rated"
+  render() {
+    const { selectedType } = this.state,
+      isNowShowingSelected = selectedType === "now_showing",
+      isTopRatedSelected = selectedType === "top_rated";
 
-		return (
-			<div>
-				<span
-					className={`tab ${isNowShowingSelected ? "selected" : ""}`}
-					onClick={this.onTabChange}
-					data-type="now_showing"
-				>
-					Now Showing
-				</span>
-				<span
-					className={`tab ${isTopRatedSelected ? "selected" : ""}`}
-					onClick={this.onTabChange}
-					data-type="top_rated"
-				>
-					Top Rated
-				</span>
-			</div>
-		)
-	}
+    return (
+      <div>
+        <span
+          className={`tab ${isNowShowingSelected ? "selected" : ""}`}
+          onClick={this.onTabChange}
+          data-type="now_showing"
+        >
+          Now Showing
+        </span>
+        <span
+          className={`tab ${isTopRatedSelected ? "selected" : ""}`}
+          onClick={this.onTabChange}
+          data-type="top_rated"
+        >
+          Top Rated
+        </span>
+      </div>
+    );
+  }
 }
 
-export default TabBar
+export default TabBar;
